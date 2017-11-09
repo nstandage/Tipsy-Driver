7a4cc1f572b21427f61b33da95cfae0eada0d59f
Nathan Standage <nathanstandage@gmail.com>
2017-11-09 15:44:26 -0700

    cleaned up UI and also made it work with iPhone X. Added alert when data isn't entered correctly.

--- Tipsy Driver/AddEntryViewController.swift
+++ Tipsy Driver/AddEntryViewController.swift
@@ -18,12 +18,12 @@ class AddEntryViewController: UIViewController {
     @IBOutlet weak var hoursTextField: UITextField!
     @IBOutlet weak var tipsTextField: UITextField!
     
+    
     weak var delegate: newEntryDelegate?
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         hoursTextField.becomeFirstResponder()
         // Do any additional setup after loading the view.
     }
@@ -33,47 +33,46 @@ class AddEntryViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    @IBAction func addNewItemButton(_ sender: UIBarButtonItem) {
-     
-
-       
-        if let hourString = hoursTextField.text, let tipString = tipsTextField.text {
-           
-            let date = Date()
-            let hours = NSString(string: hourString).doubleValue
-            let tips = NSString(string: tipString).doubleValue
-            
-            let entry = Entry(hoursWorked: hours, tips: tips, date: date)
-            
-            delegate?.addNewEntry(entry)
-            
-            self.dismiss(animated: true)
-        }
+    @IBAction func cancelButton(_ sender: Any) {
         
-    }
-    
-    @IBAction func cancelButton(_ sender: UIBarButtonItem) {
         
         hoursTextField.text = ""
         tipsTextField.text = ""
-    
-        self.dismiss(animated: true, completion: nil)
         
+        self.dismiss(animated: true, completion: nil)
     }
     
-    func updateDisplayInViewController() {
+    @IBAction func addItemButton() {
+        
+
+        if let hourString = hoursTextField.text, let tipString = tipsTextField.text {
+            
+            if hourString != "" && tipString != "" {
+                let date = Date()
+                let hours = NSString(string: hourString).doubleValue
+                let tips = NSString(string: tipString).doubleValue
+                
+                let entry = Entry(hoursWorked: hours, tips: tips, date: date)
+                
+                delegate?.addNewEntry(entry)
+                
+                self.dismiss(animated: true)
+            } else {
+                
+                let incorrectInputAlertController = UIAlertController(title: "Incorrect Info", message: "Please enter valid input", preferredStyle: .alert)
+                let incorrectInputAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                incorrectInputAlertController.addAction(incorrectInputAction)
+                self.present(incorrectInputAlertController, animated: true, completion: nil)
+            }
+
+        }
+
+        
+    }
+
+    func updateDisplayInViewcontroller() {
         
         delegate?.updateDisplay()
     }
-    /*
-    // MARK: - Navigation
-     
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
